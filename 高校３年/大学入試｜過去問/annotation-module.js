aeeefa0551aebb23d0eb0abbfd39b51d71e5b96b
Tetsuya Harashima <slimtetto@Tetsuyas-MacBook-Pro.local>
2026-01-23 21:10:16 +0900

    Auto-sync: data.json,"\351\253\230\346\240\241\357\274\223\345\271\264/\345\244\247\345\255\246\345\205\245\350\251\246\357\275\234\351\201\216\345\216\273\345\225\217/annotation-module.js"

--- 高校３年/大学入試｜過去問/annotation-module.js
+++ 高校３年/大学入試｜過去問/annotation-module.js
@@ -227,7 +227,7 @@
     const clearBtn = document.createElement('button');
     clearBtn.className = 'ann-btn';
     clearBtn.dataset.action = 'clear';
-    clearBtn.title = 'Clear All';
+    clearBtn.title = 'Clear (hold 1.5s for ALL sections)';
     clearBtn.appendChild(createSVG('M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12z', 20));
     tools.appendChild(clearBtn);
 
